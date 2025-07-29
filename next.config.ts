640675f6cb5018ad4d0b791b4282bd8cc801b856
InuYashaMX <paul@sacscloud.com>
2025-07-28 22:31:21 -0500

    Se añadieron patrones remotos para la carga de imágenes en la configuración de Next.js, permitiendo la integración de imágenes desde 'lavanguardia.com', 'images.unsplash.com' y 'via.placeholder.com'. Esto mejora la flexibilidad en la gestión de recursos visuales en la aplicación.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.lavanguardia.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
